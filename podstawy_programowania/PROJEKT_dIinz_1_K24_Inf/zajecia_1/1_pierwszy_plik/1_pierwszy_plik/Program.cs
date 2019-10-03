5f4e6ae4413849a024b09aa5a70787acd71b015b
szyper <zskgit@gmail.com>
2019-10-03 12:09:00 +0200

    first c#

---
+++ podstawy_programowania/PROJEKT_dIinz_1_K24_Inf/zajecia_1/1_pierwszy_plik/1_pierwszy_plik/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_pierwszy_plik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("WSB");
+            Console.WriteLine("Nowa linia");
+
+            Console.Write("Proszę podaj swoje imię: ");
+
+            //typ string
+            string name = Console.ReadLine();
+
+            Console.Write("Masz na imię: ");
+            Console.WriteLine(name);
+
+            //typ integer
+            int age = 20;
+            Console.WriteLine(age);
+
+            uint number = 10;
+
+            //typ float
+            float z = 14.55F;
+            //+ konkatenacja
+            Console.WriteLine("Zmienna z wynosi: " + z);
+
+            Console.WriteLine("Program obliczający pole prostokąta");
+
+
+            Console.Write("Podaj a: ");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("Podaj b: ");
+
+            string y = Console.ReadLine();
+            float bokB = float.Parse(y);
+
+            //float wynik = x * bokB;
+            //Console.WriteLine("Pole wynosi: " + wynik);
+
+            //x *= bokB; // x = x * bokB
+            //Console.WriteLine("Pole wynosi: " + x);
+
+            Console.WriteLine("Pole wynosi: " + x * bokB);
+
+            int j, k = 10, l;
+
+            //int zmienna = 10;
+            int zmienna = new int();
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
