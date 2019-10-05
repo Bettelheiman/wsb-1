8355235a3d8eaaf36734034ef33e6190128f0758
szyper <zskgit@gmail.com>
2019-10-05 11:28:49 +0200

    firstfile

---
+++ podstawy_programowania/zInz_1_K38.2_Inf/1_first_project/1_first_project/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace _1_first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //komentarz
+            Console.Write("Imię: "); //wyświetli Imię
+            Console.WriteLine("Janusz");
+            /* komentarz
+             * w wielu
+             * liniach
+             */
+
+            /*
+             *   #
+             *   ##
+             *   ###
+             */
+            Console.WriteLine("#\n##\n###");
+
+            string name;
+            name = "Anna";
+
+            Console.WriteLine(name);
+            //+ konkatenacja
+            Console.WriteLine("Masz na imię: " + name);
+            Console.WriteLine("Masz na imię: {0}",name);
+
+            int age = 20;
+            Console.WriteLine("Masz na imię: {0}, wiek: {1}",name,age);
+
+            sbyte age1 = 20;
+            Console.WriteLine("Masz na imię: {0}, wiek1: {1}", name, age1);
+
+            Console.ReadKey();
+        }
+    }
+}
